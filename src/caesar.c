2c7354510d8c2e2a282119c9d93e5666b90d5719
ADong-01 <andy.dong.1@stonybrook.edu>
2025-10-01 14:02:07 -0400

    commit test (unfinished)

--- src/caesar.c
+++ src/caesar.c
@@ -1,26 +1,116 @@
 #include <stdlib.h>
 #include "caesar.h" 
 #include "strPtr.h"
-
+// ASCII table: 
+// numbers: 48-57 0 to 9
+// LetterU: 65-90
+// LetterL: 97-122
 /**
  *  Feel free to use the functions that you made in strPtr.c
 */ 
 
+/* Understanding: 
+return -2 if NULLs 
+Letters shift forward (ASCII change) by (key + index) % 26 in respective upper/lower case
+Digits shift forward (ASCII change) by (key +2*index) % 10 in digits
+All else stays the same and does not increment count of encoded characters
+Append on the end of ciphertext "__EOM___" and '\0'
+Return count of incremented characters 
+*/
 int encryptCaesar(const char *plaintext, char *ciphertext, int key) {
+    if(plaintext == NULL || ciphertext == NULL)
+        return -2;
 	// TODO: implement
-    // test test test upd
+    // test test test
+    int i = 0;
+    int increments = 0;
+    while(plaintext[i] != '\0'){
+        if(plaintext[i] >= 65 && plaintext[i] <= 90){ // Upper case, or just do 'A'
+            int shiftKey = (key + i) % 26;
+            int alphabet = (shiftKey + plaintext[i] - 65) % 26;
+            ciphertext[i] = alphabet + 65;
+            increments++;
+           // i++;
+            //continue;
+        }
+        else if(plaintext[i] >= 97 && plaintext[i] <= 122){ // Lower case
+            int shiftKey = (key + i) % 26;
+            int alphabet = (shiftKey + plaintext[i] - 97) % 26;
+            ciphertext[i] = alphabet + 97;
+            increments++;
+            //    i++;
+        }
+        else if(plaintext[i] >= 48 && plaintext[i] <= 57){
+            int shiftKey = ((key + (2 * i)) % 10);
+            int numbers = (shiftKey + plaintext[i] - 48) % 10;
+            ciphertext[i] = numbers + 48;
+            increments++;
+        }
+        else 
+            ciphertext[i] = plaintext[i];
+        i++;
+    }
+
     (void)plaintext;
     (void)ciphertext;
-    (void)key;
-    return 0;
+    (void)key;//
+
+    char add[] = "__EOM__";
+    int k = 0;
+    while(add[k] != '\0'){
+        ciphertext[i] = add[k];
+        i++;
+        k++;
+    }
+    ciphertext[i] = '\0';
+
+    return increments;
 }
 
 int decryptCaesar(const char *ciphertext, char *plaintext, int key) {
+    if(ciphertext == NULL || plaintext == NULL)
+        return -2;
 	// TODO: implement
+    int i = 0;
+    int increments = 0;
+    while(ciphertext[i] != '\0'){
+        if(ciphertext[i] == '_' && ciphertext[i+1] == '_' &&
+        ciphertext[i+2] == 'E' && ciphertext[i+3] == 'O' && ciphertext[i+4] == 'M' &&
+        ciphertext[i+5] == '_' && ciphertext[i+6] == '_')
+            goto next;
+        else 
+            if(ciphertext[i+1] == '\0')
+                return -1;
+            else
+                i++;
+    }
+    next:
+    for(int k = 0; k < i; k++){
+        if(ciphertext[i] >= 65 && ciphertext[i] <= 90){ // Upper case, or just do 'A'
+            int shiftKey = (key + i) % 26;
+            int alphabet = (ciphertext[i] - 65 + 26 - shiftKey) % 26;
+            plaintext[i] = alphabet + 65;
+            increments++;
+        }
+        else if(ciphertext[i] >= 97 && ciphertext[i] <= 122){ // lower case, or just do '
+            int shiftKey = (key + i) % 26;
+            int alphabet = (ciphertext[i] - 97 + 26 - shiftKey) % 26;
+            plaintext[i] = alphabet + 97;
+            increments++;
+        }
+        else if(ciphertext[i] >= 48 && ciphertext[i] <= 57){
+            int shiftKey = ((key + (2 * i)) % 10);
+            int numbers = (ciphertext[i] - 48 + 10 - shiftKey) % 10;
+            plaintext[i] = numbers + 48;
+            increments++;
+        }
+    }
+    plaintext[i] = '\0';
+
     (void)ciphertext;
     (void)plaintext;
     (void)key;
-    return 0;
+    return increments;
 }
 
 
@@ -37,8 +127,35 @@ int main(int argc, char* argv[]){
 	(void)argc;
 	(void)argv;
 	/** CREATE TEST CASES HERE **/
+    // // // // // ENCRYPT
+    char ciphertext0[27] = {0};
+    printf("%d\n", encryptCaesar("System Fundamentals", ciphertext0, 1));
+    printf("%s\n", ciphertext0);
 
+    char ciphertext1[11] = {0};
+    printf("%d\n", encryptCaesar("abc", ciphertext1, 2));
+    printf("%s\n", ciphertext1);
 
+    char ciphertext2[11] = {0};
+    printf("%d\n", encryptCaesar("Ayb", ciphertext2, 3));
+    printf("%s\n", ciphertext2);
+
+    char ciphertext3[14] = {0};
+    printf("%d\n", encryptCaesar("Cse220", ciphertext3, 1));
+    printf("%s\n", ciphertext3);
+
+    char ciphertext4[10] = {0};
+    printf("%d\n", encryptCaesar("CS", ciphertext4, 0));
+    printf("%s\n", ciphertext4);
+
+    char ciphertextX[8] = {0};
+    printf("%d\n", encryptCaesar("", ciphertextX, 28));
+    printf("%s\n", ciphertextX); //fix undefined case, empty input???
+    
+    // // // // // DECRYPT
+    char plaintext0[3] = {0};
+    printf("%d\n", decryptCaesar("ceg__EOM__", plaintext0, 2));
+    printf("%s\n", plaintext0); 
 
 	
 	/** ---------------------- **/
--- src/strPtr.c
+++ src/strPtr.c
@@ -1,41 +1,133 @@
 #include <stdlib.h>
 #include "strPtr.h"
-
-#include <stdlib.h>
-#include "strPtr.h"
+// cleared extra??
+// ASCII table: 
+// numbers: 48-57 0 to 9
+// LetterU: 65-90
+// LetterL: 97-122
 
 int strgLen(const char *s) {
+    if(s == NULL)
+        return -1;
     // TODO: implement
-    return 0;
+    int length = 0;
+    while(s[length] != '\0'){
+        length++;
+    }
+
+    return length;
 }
 
-void strgCopy(char *dest, char *src) {
-    // TODO: implement
-    (void)dest;
-    (void)src;
+void strgCopy(char *s, char *d) { //change parameter order??
+    if(s == NULL || d ==  NULL)
+        return;
+    // TODO: implement can simplify, pointer iteration
+    for(;;){
+        *d = *s;
+        if(*s == '\0')
+            break;
+        d++; s++;
+    }
+    (void)d;
+    (void)s;
 }
 
 void strgChangeCase(char *s) {
+    if(s == NULL)
+        return;
     // TODO: implement
+    // ONLY IF letters only, not adjacent to digits
+    int i = 0;
+    while(s[i] != '\0'){
+        int digitBool = !((i>=1 && s[i-1]>='0' && s[i-1]<='9')
+                        || (s[i+1]!='\0' && s[i+1]>='0' && s[i+1]<='9' ));
+        // 0 if adjacent digit(s), negated to 1
+        if(s[i] >= 'A' && s[i] <= 'Z' && digitBool)// upper
+            s[i] = s[i] + (32);
+        else if(s[i] >= 'a' && s[i] <= 'z' && digitBool)// lower
+            s[i] = s[i] - (32);
+        i++;
+
+    }
     (void)s;
 }
 
 int strgDiff(char *s1, char *s2) {
+    if(s1 == NULL || s2 == NULL)
+        return -2;
     // TODO: implement
+    int i = 0;
+    while(*s1 != '\0' && *s2 != '\0'){
+        if((*s1 != *s2))
+            return i;
+        s1++;
+        s2++;
+        i++;
+    }
+    // different lengths have early loop exit, != || !=
+    if (*s1 != *s2)
+        return i;
+    else 
+        return -1;
     (void)s1;
     (void)s2;
-    return 0;
 }
 
 void strgInterleave(char *s1, char *s2, char *d) {
+    if(s1 == NULL || s2 == NULL || d == NULL)
+        return;
     // TODO: implement
+    int i = 0;
+    while((*s1 != '\0' || *s2 != '\0')){
+        if(*s1 == '\0'){
+            d[i] = *s2;
+            s2++;
+        }
+        else if(*s2 == '\0'){
+            d[i] = *s1;
+            s1++;
+        }
+        else if(i % 2 == 0){
+            d[i] = *s1;
+            s1++;
+        }
+        else if(i % 2 == 1){
+            d[i] = *s2;
+            s2++;
+        }
+        i++;
+    }
     (void)s1;
     (void)s2;
     (void)d;
+    d[i] = '\0';
 }
 
 void strgReverseLetters(char *s) {
     // TODO: implement
+    int length = 0;
+    while(s[length] != '\0'){
+        length++;
+    }
+    int ptrL = 0;
+    int ptrR = length - 1;
+    while(ptrL < ptrR){
+        if((s[ptrL] >= 'A' && s[ptrL] <= 'Z') || (s[ptrL] >='a' && s[ptrL] <= 'z')){
+            if((s[ptrR] >= 'A' && s[ptrR] <= 'Z') || (s[ptrR] >='a' && s[ptrR] <= 'z')){
+                char swap = s[ptrL];
+                s[ptrL] = s[ptrR];
+                s[ptrR] = swap;
+                ptrL++;
+                ptrR--;
+                continue;
+            }
+        }
+        if(!((s[ptrL] >= 'A' && s[ptrL] <= 'Z') || (s[ptrL] >='a' && s[ptrL] <= 'z')))
+            ptrL++;
+        if(!((s[ptrR] >= 'A' && s[ptrR] <= 'Z') || (s[ptrR] >='a' && s[ptrR] <= 'z')))
+            ptrR--;
+        
+    }
     (void)s;
 }
 
@@ -54,7 +146,93 @@ int main(int argc, char* argv[]){
 	(void)argc;
 	(void)argv;
 	/** CREATE TEST CASES HERE **/
-	
+	printf("%d\n", strgLen("Stony Brook"));
+    printf("%d\n", strgLen("CSE 220"));
+    printf("%d\n", strgLen("C"));
+    printf("%d\n", strgLen("System Fundamental"));
+    printf("%d\n", strgLen("1"));
+    printf("%d\n", strgLen(""));
+    printf("%d\n", strgLen(NULL));
+
+    char destination[99];
+    strgCopy("Computer Science", destination);
+    printf("%s\n", destination);
+    strgCopy("CSE-220", destination);
+    printf("%s\n", destination);
+    strgCopy("System Fundamental", destination);
+    printf("%s\n", destination);
+    strgCopy("1", destination);
+    printf("%s\n", destination);
+    strgCopy("", destination);
+    printf("%s\n", destination);
+    strgCopy(NULL, NULL);
+    printf("%s\n", destination);
+
+    char s1[] = "Stony Brook";
+    strgChangeCase(s1);
+    printf("%s\n", s1);
+    char s2[] = "CSE220";
+    strgChangeCase(s2);
+    printf("%s\n", s2);
+    char s3[] = "a1b";
+    strgChangeCase(s3);
+    printf("%s\n", s3);
+    char s4[] = "System Fundamental220";
+    strgChangeCase(s4);
+    printf("%s\n", s4);
+    char s5[] = "1";
+    strgChangeCase(s5);
+    printf("%s\n", s5);
+    char s6[] = "";
+    strgChangeCase(s6);
+    printf("%s\n", s6);
+    char *s7 = NULL;
+    strgChangeCase(s7);
+    printf("%s\n", s7);
+
+    printf("%d\n", strgDiff("Hello", "Hello"));
+    printf("%d\n", strgDiff("CSE-220", "CSE220)"));
+    printf("%d\n", strgDiff("CSE220", "SE220"));
+    printf("%d\n", strgDiff("", ""));
+    printf("%d\n", strgDiff("Hi", NULL));
+
+    char destination2[99];
+    strgInterleave("abc", "123", destination2);
+    printf("%s\n", destination2);
+    strgInterleave("abcdef", "123", destination2);
+    printf("%s\n", destination2);
+    strgInterleave("cse", "12345", destination2);
+    printf("%s\n", destination2);
+    strgInterleave("1234", "cs", destination2);
+    printf("%s\n", destination2);
+    strgInterleave("", "", destination2);
+    printf("%s\n", destination2);
+    strgInterleave("", "123", destination2);
+    printf("%s\n", destination2);
+    strgInterleave(NULL, "123", destination2);
+    printf("%s\n", destination2); // did nothing, so it prints last d statement
+    // incorrect, continuous string
+    char testShort[5];
+    testShort[4] = '\0';
+    strgInterleave("Tea", "Cup", testShort);
+    printf("%s\n", testShort); // IGNORE, TA explains in Piazza to ignore short byte condition.
+
+    char st1[] = "Hello";
+    strgReverseLetters(st1);
+    printf("%s\n", st1);
+    char st2[] = "ab-cd";
+    strgReverseLetters(st2);
+    printf("%s\n", st2);
+    char st3[] = "a1b2c";
+    strgReverseLetters(st3);
+    printf("%s\n", st3);
+    char st4[] = "a-sd-be1";
+    strgReverseLetters(st4);
+    printf("%s\n", st4);
+    char st5[] = "abcd----";
+    strgReverseLetters(st5);
+    printf("%s\n", st5);
+
 	/** ---------------------- **/
 	return 0;
 }
